7130a16ce06405076aa840cb3098fd029c93b25c
figaroyang <yang.figaro0507@gmail.com>
2025-04-28 17:49:45 +0800

    test triangle

--- E2A30_HW2.cpp
+++ E2A30_HW2.cpp
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <conio.h>
 
+int n;
 int main(void)
 {
+    system("chcp 65001");
     puts("==========================================================================");
     puts("                    Welcome to Your Personalized UI!                     ");
     puts("|----------------------------------------------------------------------");
@@ -33,9 +36,8 @@ int main(void)
     puts(word);
     strcat(word, "o");
     puts(word);
-    return 0;
     int i, password = 2025;
-    char ch1, ch2, j, tt;
+    char ch1, ch2, j;
     int k, time = 0, num, y = 1, z;
     for (i = 1; i <= 3; i++)
     {
@@ -66,4 +68,68 @@ int main(void)
         printf("| c. 結束 |\n");
         printf("--------------------------------\n");
         fflush(stdin);
+        printf("請輸入A-C或a-c:\n");
+        scanf("%c", &ch1);
+
+        switch (ch1)
+        {
+        case 'A':
+        case 'a':
+
+            system("cls");
+            time = 0;
+            k = 0;
+            j = 0;
+            while (1)
+            {
+                fflush(stdin);
+                printf("請輸入一個字元(a-n): \n");
+                scanf("%c", &ch2);
+                time = ch2 - 'a';
+                if (ch2 >= 'a' && ch2 <= 'n')
+                {
+                    while (k <= time)
+                    {
+                        j = 'a';
+                        while (j <= ch2)
+                        {
+                            printf("%c", j);
+                            j++;
+                        }
+                        printf("\n");
+                        k++;
+                    }
+                    break;
+                }
+                else
+                {
+                    printf("輸入錯誤，請重新輸入\n");
+                }
+            }
+            system("pause");
+            system("cls");
+            break;
+
+            
+            case 'B':
+            case 'b':
+            printf("輸入整數1~9:%d",n);
+            scanf("%d",&n);
+
+            if(n<1||n>9)
+            {
+                printf("\n乘法表 (%dx%d)",n,n);
+            }
+            for(int i=1; i<=n ; i++)
+            {
+                for(int j=1 ;j<=n ;j++)
+                {
+                    printf("%4d",i+j);
+                }
+            }
+
+            
+
+        }
     }
+}
